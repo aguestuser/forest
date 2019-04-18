9998cac7e2e1d430908a700555334454481caca8
aguestuser <aguestuser@riseup.net>
2019-04-17 20:44:22 -0400

    [WIP] finish implementing `LockedKeystore::from_password`

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,12 @@
-use sodiumoxide::crypto::pwhash::argon2id13;
-use argon2id13::{OpsLimit,MemLimit,HashedPassword};
+use box_::{PublicKey, SecretKey, PUBLICKEYBYTES, SECRETKEYBYTES};
+use pwhash::{MemLimit, OpsLimit, Salt, SALTBYTES};
+use secretbox::{Key, Nonce, KEYBYTES, NONCEBYTES};
+use sodiumoxide::crypto::{box_, pwhash, secretbox};
 
-// Technology for Encrypted Resting Data Storage
-// TERDS
+// Technology for Encrypted Resting Data
 
-// Technology for Resting Email Encryption Storage
-
-
-/**
- * consume: password
- * generate:
- * - encrypted priv key
- * - nonce
- * - public key
- * ->
- *
- */
-
-const DEFAULT_OPSLIMIT: OpsLimit = argon2id13::OPSLIMIT_MODERATE;
-const DEFAULT_MEMLIMIT: MemLimit = argon2id13::MEMLIMIT_MODERATE;
+const DEFAULT_OPSLIMIT: OpsLimit = pwhash::scryptsalsa208sha256::OPSLIMIT_INTERACTIVE;
+const DEFAULT_MEMLIMIT: MemLimit = pwhash::scryptsalsa208sha256::MEMLIMIT_INTERACTIVE;
 
 type HashParams = (OpsLimit, MemLimit);
 
@@ -28,12 +16,10 @@ enum Keystore {
 }
 
 struct LockedKeystore {
-    public_key: Vec<u8>,
-    encrypted_secret_key: Vec<u8>,
-    secret_key_nonce: Vec<u8>, // FIXME: key size?
-    pwhash_opslimit: OpsLimit,
-    pwhash_memlimit: MemLimit,
-    pwhash_salt: Vec<u8>,
+    pk_bytes: [u8; PUBLICKEYBYTES],
+    encrypted_sk: Vec<u8>,
+    sk_nonce_bytes: [u8; NONCEBYTES],
+    pwhash_salt_bytes: [u8; SALTBYTES],
 }
 
 struct UnlockedKeystore {
@@ -48,17 +34,43 @@ impl Drop for UnlockedKeystore {
 }
 
 impl LockedKeystore {
-    fn from_password(password: &[u8], hash_params: Option<HashParams>) -> Result<LockedKeystore, ()> {
-        let (opslimit, memlimit) = hash_params.unwrap_or((DEFAULT_OPSLIMIT, DEFAULT_MEMLIMIT));
-        let pwhash = argon2id13::pwhash(password, opslimit, memlimit)?;
-        
+    fn from_password(password: &[u8]) -> Result<LockedKeystore, ()> {
+        let (PublicKey(pk_bytes), SecretKey(sk_bytes)) = box_::gen_keypair();
+        let (Salt(pwhash_salt_bytes), Nonce(sk_nonce_bytes)) =
+            (pwhash::gen_salt(), secretbox::gen_nonce());
+        let secretbox_key = password_kdf(password, &Salt(pwhash_salt_bytes))?;
+
+        Ok(LockedKeystore {
+            pk_bytes,
+            sk_nonce_bytes,
+            pwhash_salt_bytes,
+            encrypted_sk: secretbox::seal(&sk_bytes, &Nonce(sk_nonce_bytes), &secretbox_key),
+        })
     }
+
+}
+
+fn password_kdf(password: &[u8], salt: &Salt) -> Result<Key, ()> {
+    let mut key = Key([0; secretbox::KEYBYTES]);
+    let Key(ref mut key_bytes) = key;
+    pwhash::derive_key(
+        key_bytes,
+        password,
+        salt,
+        DEFAULT_OPSLIMIT,
+        DEFAULT_MEMLIMIT,
+    )?;
+    Ok(key)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_pwhash() {
-        
+    fn test_constructing_locked_keystore_from_password() {
+        let password = String::from("open sesame");
+        let locked_keystore = LockedKeystore::from_password(password.as_bytes());
+        assert!(true);
     }
 }
